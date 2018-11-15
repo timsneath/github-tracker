e43439bf9fe64766862bcd8e29991aa64c74655e
Tim Sneath <timsneath@google.com>
2018-11-15 09:50:28 -0800

    added a few more content repos

--- repo-stars.dart
+++ repo-stars.dart
@@ -51,7 +51,9 @@ final List contentRepos = [
   'awesomedata/awesome-public-datasets',
   'tiimgreen/github-cheat-sheet',
   'CyC2018/Interview-Notebook',
-  'CyC2018/CS-Notes'
+  'CyC2018/CS-Notes',
+  'kdn251/interviews',
+  'k88hudson/git-flight-rules',
 ];
 
 Future main(List<String> args) async {
